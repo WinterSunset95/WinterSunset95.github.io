89b26994232e1d670dcf28ed113dccc8bfb38286
WinterSunset95 <wintersunset95@gmail.com>
2022-03-17 19:27:31 +0000

    starting the project

--- react-development/src/App.js
+++ react-development/src/App.js
@@ -1,7 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+import Main from './components/Main'
 
 function App() {
   return (
-    <h1>Wassup mofo</h1>
+    <div className="container">
+      <Header />
+      <Main />
+    </div>
   );
 }
 
