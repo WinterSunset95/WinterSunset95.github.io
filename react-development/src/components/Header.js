6108aeb5f5622eb4bf8262a851f85d670f618506
WinterSunset95 <wintersunset95>
2022-04-12 13:12:51 +0530

    trial

--- react-development/src/components/Header.js
+++ react-development/src/components/Header.js
@@ -4,14 +4,14 @@ import Logo from '../images/Logo.jpg'
 export default function Header() {
     return (
         <nav className="row nav border align-items-center">
-            <div className='row align-items-center col-6 header_userInfo'>
-                <img src={Logo} alt="Winter's display pic" className="img-thumbnail float-start rounded" />
-                <h2 className="header_name">WinterSunset95</h2>
+            <div className='align-items-center col-6'>
+                <img src={Logo} width="50px" alt="Winter's display pic" className="rounded-circle" />
+                <h2 className="font-size-md">WinterSunset95</h2>
             </div>
-            <ul className="col-6 nav justify-content-end nav-pills">
-                <li className="nav-item"><a href="" className="nav-link active">Home</a></li>
-                <li className="nav-item"><a href="" className="nav-link">Projects</a></li>
-                <li className="nav-item"><a href="" className="nav-link">Games</a></li>
+            <ul className="col-6 nav d align-items-center justify-content-end nav-pills">
+                <li className="nav-item "><a href="" className="nav-link active">Home</a></li>
+                <li className="nav-item "><a href="" className="nav-link">Projects</a></li>
+                <li className="nav-item "><a href="" className="nav-link">Games</a></li>
             </ul>
         </nav>
     )
