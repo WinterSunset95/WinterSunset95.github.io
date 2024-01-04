d1495b99cab0984914aa6caa1610ffefa41e6b6c
WinterSunset95 <wintersunset95@gmail.com>
2024-01-04 20:42:53 +0530

    Done for the day

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const content = document.getElementById("content")
 
-const homeUrl = "/wintersunset95.github.io"
+const homeUrl = ""
 
 // Fetch header
 fetch(`${homeUrl}/src/header.html`)
@@ -24,33 +24,3 @@ fetch(`${homeUrl}/src/footer.html`)
 })
 
 
-// This is the TIL content
-const latest = document.getElementById("latest")
-
-function getTil(filename) {
-	fetch(`${homeUrl}/til/${filename}.html`)
-	.then(response => response.text())
-	.then(data => {
-		const div = document.createElement("div")
-		div.innerHTML = data
-		console.log(div)
-		const title = div.querySelector("h1").innerText
-		const intro = div.querySelector("#intro").innerText
-		const toAppend = document.createElement("div")
-		toAppend.innerHTML = `
-		<a href="${homeUrl}/til/index.html?id=filename">
-			<h3>${title}</h3>
-		</a>
-		<p>${intro}</p>
-		`
-		latest.appendChild(toAppend)
-	})
-}
-
-fetch(`${homeUrl}/til/til.json`)
-.then(response => response.json())
-.then(data => {
-	data.forEach(name => {
-		getTil(name)
-	})
-})
